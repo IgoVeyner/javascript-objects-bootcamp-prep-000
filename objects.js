a1f66a98d5f0868f7f0921812af39bfb3f1abeb8
Igor Veyner <igorveyner95@gmail.com>
2020-01-26 14:20:27 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 const playlist = {'My Bloody Valentine':'Slowdrive'}
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist.artistName = songTitle
+  playlist[artistName] = songTitle
   return playlist
 }
